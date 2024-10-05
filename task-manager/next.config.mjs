9e0ba159da6595ea045e9bdf93619d7bffe7169f
Jade Abidi <jabi0002@student.monash.edu>
2024-10-05 16:35:27 +1000

    perms

--- task-manager/next.config.mjs
+++ task-manager/next.config.mjs
@@ -2,3 +2,19 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com",
+            },
+          ],
+        },
+      ];
+    },
+  };
