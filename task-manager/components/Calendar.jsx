a3cf3c924536bc24b6eb41f9e72893d523f9a797
Jade Abidi <jabi0002@student.monash.edu>
2024-10-08 20:31:49 +1100

    remove logging

--- task-manager/components/Calendar.jsx
+++ task-manager/components/Calendar.jsx
@@ -196,14 +196,6 @@ const Calendar = ({ groupID }) => {
 
       const timezoneOffset = new Date().getTimezoneOffset() * 60000;
 
-      console.log(calendarDays);
-      const day = calendarDays[15];
-      console.log(day.getDate());
-      console.log(day.getUTCDate());
-      console.log(new Date(events[0].start_date).getDate());
-      console.log(new Date(events[0].start_date).getUTCDate());
-      console.log(`Timezone offset: ${timezoneOffset}`);
-
       console.log(
         new Date(
           new Date(event["start_date"]).getTime() - timezoneOffset
